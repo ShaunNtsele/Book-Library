24e47d49d5e3dad3a192d25bfe7ffb3595d77b1a
Shaun Ntsele <2017217726@ufs4life.ac.za>
2023-07-04 14:47:14 +0200

    Library Controllers Setup

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,122 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Book_Library.Models
+{
+[Authorize]
+[Route("[controller]")]
+[ApiController]
+public class AuthorsController : ControllerBase
+{
+
+
+    private readonly IRepositoryWrapper _repo;
+    private readonly UserManager<IdentityUser> _userManager;
+
+
+    public AuthorsController(IRepositoryWrapper repo, UserManager<IdentityUser> userManager)
+    {
+        _repo = repo;
+        _userManager = userManager;
+
+
+    }
+
+
+    [HttpPost]
+    //create new author
+    public IActionResult Create(Author author)
+    {
+        //get details of logged in user
+        var currentUser = _userManager.FindByNameAsync(User.Identity.Name);
+        var userId = currentUser.Result.Id;
+
+        if (ModelState.IsValid)
+        {
+
+            _repo.Author.Create(author);
+            author.CreatedBy = Guid.Parse(userId);
+            _repo.Save();
+            return Ok(author);
+        }
+        else
+        {
+            return StatusCode(StatusCodes.Status400BadRequest);
+        }
+
+    }
+
+    [HttpGet]
+    [Route("{authorId}")]
+    //return a specific author
+    public IActionResult Read(Guid authorId)
+    {
+        var author = _repo.Author.GetById(authorId);
+        return Ok(author);
+    }
+
+    [HttpGet]
+    [Route("")]
+    //return list of all authors
+    public IActionResult Read()
+    {
+        var listAuthors = _repo.Author.Read();
+        return Ok(listAuthors);
+    }
+
+    [HttpDelete]
+    [Route("{authorId}")]
+
+    //delete specific author
+    public IActionResult Delete(Guid authorId)
+    {
+        var author = _repo.Author.GetById(authorId);
+        var currentUser = _userManager.FindByNameAsync(User.Identity.Name);
+        var userId = currentUser.Result.Id;
+        if (author == null)
+            return StatusCode(StatusCodes.Status404NotFound, new Response { Status = "Error", Message = "Author does not exist" });
+        else
+        {
+            if (author.CreatedBy == Guid.Parse(userId))
+            {
+                _repo.Author.Delete(author);
+                _repo.Save();
+                return Ok();
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, new Response { Status = "Error", Message = "You are not authorised to delete the user" });
+            }
+
+        }
+
+    }
+
+    [HttpPut]
+    [Route("{authorId}")]
+    public IActionResult Update(Author author)
+    {
+        var currentUser = _userManager.FindByNameAsync(User.Identity.Name);
+        var userId = currentUser.Result.Id;
+        if (author != null)
+        {
+            if (author.CreatedBy == Guid.Parse(userId))
+            {
+                _repo.Author.Update(author);
+                _repo.Save();
+                return Ok(author);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, new Response { Status = "Error", Message = "You are not authorised to update the user" });
+            }
+
+        }
+        else
+        {
+            return StatusCode(StatusCodes.Status204NoContent);
+        }
+    }
+}
+}
---
+++ Controllers/BooksController.cs
@@ -0,0 +1,104 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Book_Library.Models
+{
+[Authorize]
+[Route("[controller]")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+
+
+    private readonly IRepositoryWrapper _repo;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public BooksController(IRepositoryWrapper repo, UserManager<IdentityUser> userManager)
+    {
+        _repo = repo;
+        _userManager = userManager;
+    }
+
+    [HttpPost]
+    [Route("{authorId}")]
+    //create new book
+    public IActionResult Create(Book book, Guid authorId)
+    {
+        if (ModelState.IsValid)
+        {
+            var currentUser = _userManager.FindByNameAsync(User.Identity.Name);
+            var userId = currentUser.Result.Id;
+            _repo.Book.Create(book);
+            book.CreatedBy = Guid.Parse(userId); 
+            var author = _repo.Author.GetById(authorId);
+            book.Author = author;
+            _repo.Save();
+
+
+            return Ok(book);
+        }
+        else
+        {
+            return StatusCode(StatusCodes.Status400BadRequest);
+        }
+    }
+
+    [HttpGet]
+   
+    //return list of all books
+    public IActionResult Read()
+    {
+
+        //var listBooks = _repo.Book.Read();
+        var listBooks = _repo.Book.GetBooksWithAuthorDetails();
+        List<object> bookDetails = new List<object>(listBooks.Count());
+        foreach(Book book in listBooks)
+        {
+            
+            bookDetails.Add(new
+             {
+                bookName = book.BookName, 
+                bookAuthorName = book.Author.AuthorName, 
+                ownsAuthor = book.CreatedBy == book.Author.CreatedBy
+            });
+        }
+        return Ok(bookDetails);
+    }
+
+    [HttpGet]
+    [Route("{authorId}")]
+    //return a specific book
+    public IActionResult Read(Guid authorId)
+    {
+        var book = _repo.Book.GetBookWithAuthorDetails(authorId);
+        var bookDetails = new 
+        {
+            bookName = book.BookName,
+            datePublished = book.DatePublished
+        };
+        return Ok(bookDetails);
+    }
+
+    [HttpGet]
+    [Route("{authorId}/{bookId}")]
+    public IActionResult Read(Guid authorId, Guid bookId)
+    {
+        Book book = _repo.Book.GetBookWithAuthorDetails(authorId);
+         var creatorName = _userManager.FindByIdAsync(book.CreatedBy.ToString()); 
+        
+        
+        var bookDetails = new 
+        {
+            bookName = book.BookName, 
+            datePublished = book.DatePublished,
+            publisher = book.Publisher,
+            copiesSold = book.copiesSold,
+            creatorName = creatorName.Result.UserName
+        };
+       
+        return Ok(bookDetails);
+    }
+
+}
+}
